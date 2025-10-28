54b219f7baaea0b60952b8a9dacb130983148cd5
cesar luiz da silva <cesarluizdasilva10@gmail.com>
2025-10-28 19:47:46 -0300

    interação com dados dos funcionarios

---
+++ src/main/java/com/trabalho/Restaurante/core/repository/FuncionarioRepository.java
@@ -0,0 +1,19 @@
+package com.trabalho.Restaurante.core.repository;
+
+import java.util.Optional;
+
+import com.trabalho.Restaurante.core.entity.Funcionario;
+
+public class FuncionarioRepository {
+
+    public Optional<Funcionario> findByEmail(String email) {
+        // 
+        throw new UnsupportedOperationException("Unimplemented method 'findByEmail'");
+    }
+
+    public Funcionario save(Funcionario funcionario) {
+        // 
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+    
+}
